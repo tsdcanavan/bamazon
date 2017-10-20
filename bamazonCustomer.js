34dbb184edfdafd7b25dc65bd979f546a59140a2
unknown <tsdcanavan@yahoo.com>
2017-10-19 21:16:44 -0400

    list database working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,40 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-var tables = require("tables");
+var table = require("table");
+
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'bamazon'
-  });
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+});
 
 function endConnect() {
     connection.end();
 }
 
 function listAllItems() {
-    mysql("SELELCT * FROM products");
+    console.log("Selecting all records...");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        res.forEach(function (element) {
+            console.log(element.item_id,
+                 element.product_name,
+                 element.department_name,
+                 element.price,
+                 element.stock_quantity);
+        });
+    });
 }
+
+function selectItem() {
+
+};
+
+function postItem() {
+
+};
 
 listAllItems();
 endConnect();
